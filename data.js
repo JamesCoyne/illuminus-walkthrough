5e3b702405b6c3b05d1ceaaae450fadb007ed4bd
[Shawn <coynej1@wit.edu>
2018-10-08 15:54:23 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,469 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-11227_pano__07",
+      "name": "11227_PANO__07",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.9399054921134464,
+        "pitch": -0.37115785821067604,
+        "fov": 1.3380262414692456
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2918988378718801,
+          "pitch": 0.06177182052168817,
+          "rotation": 0,
+          "target": "1-11267_pano__01"
+        },
+        {
+          "yaw": 1.3251885753069494,
+          "pitch": 0.01962898809385294,
+          "rotation": 0,
+          "target": "3-11250_pano__05"
+        },
+        {
+          "yaw": 0.6606436795027513,
+          "pitch": -0.014051474090045701,
+          "rotation": 0,
+          "target": "5-11219_pano__06"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.25404825579391854,
+          "pitch": -0.8213772848304153,
+          "title": "Projection Surface",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.9627230921647509,
+          "pitch": -0.06685679478732887,
+          "title": "Control 1",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.9403034392651142,
+          "pitch": -0.22908760500561343,
+          "title": "Control 2",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.1255340072087243,
+          "pitch": -0.12083631828908992,
+          "title": "Control 3",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-11267_pano__01",
+      "name": "11267_PANO__01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4088284750813429,
+          "pitch": 0.21756637077046292,
+          "rotation": 0,
+          "target": "0-11227_pano__07"
+        },
+        {
+          "yaw": -0.5830108241853864,
+          "pitch": 0.11829643822052205,
+          "rotation": 0,
+          "target": "2-11255_pano__02"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3774139125694038,
+          "pitch": 0.24518648807687882,
+          "title": "Control 1",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.3590282219351657,
+          "pitch": -0.9692374616934707,
+          "title": "Projection Surface",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.475732635173415,
+          "pitch": 0.017905456344381676,
+          "title": "Control 3",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-11255_pano__02",
+      "name": "11255_PANO__02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.876004455717265,
+        "pitch": -0.1195531676141961,
+        "fov": 1.4311086604234626
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6593043656100264,
+          "pitch": 0.06449046012162896,
+          "rotation": 0,
+          "target": "1-11267_pano__01"
+        },
+        {
+          "yaw": -3.1001314619731275,
+          "pitch": 0.37508113761709794,
+          "rotation": 0,
+          "target": "3-11250_pano__05"
+        },
+        {
+          "yaw": -2.2582622556302,
+          "pitch": 0.1729055302111071,
+          "rotation": 0,
+          "target": "6-11215_pano__04"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.128862627750312,
+          "pitch": -0.2971019211662256,
+          "title": "Control 2",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.137667403930454,
+          "pitch": -0.6565993320875556,
+          "title": "Projection Surface",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.167351646367928,
+          "pitch": 0.041407953315749424,
+          "title": "Control 3",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.7793303457723724,
+          "pitch": 0.009063468556782084,
+          "title": "Control 1",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-11250_pano__05",
+      "name": "11250_PANO__05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.15966546203187093,
+        "pitch": -0.2647000148052463,
+        "fov": 1.3380262414692456
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0850978279123229,
+          "pitch": 0.1873830844196469,
+          "rotation": 0,
+          "target": "2-11255_pano__02"
+        },
+        {
+          "yaw": 1.5047090250425628,
+          "pitch": 0.1256253911390388,
+          "rotation": 0,
+          "target": "0-11227_pano__07"
+        },
+        {
+          "yaw": 2.339755905549608,
+          "pitch": 0.12217246657267111,
+          "rotation": 0,
+          "target": "1-11267_pano__01"
+        },
+        {
+          "yaw": -2.9911453145425604,
+          "pitch": 0.15106215976851978,
+          "rotation": 0,
+          "target": "5-11219_pano__06"
+        },
+        {
+          "yaw": -2.1405662758998876,
+          "pitch": 0.17327740015371518,
+          "rotation": 0,
+          "target": "6-11215_pano__04"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-11237_pano__03",
+      "name": "11237_PANO__03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.007779830699988111,
+        "pitch": 0,
+        "fov": 1.4311086604234626
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.005049576002303269,
+          "pitch": 0.10227341811226687,
+          "rotation": 0,
+          "target": "6-11215_pano__04"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.32859955224028425,
+          "pitch": -0.4585086032187746,
+          "title": "Projection Surface",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6892820732043106,
+          "pitch": -0.3814815730920902,
+          "title": "Control 2",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-11219_pano__06",
+      "name": "11219_PANO__06",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.06205615118204122,
+        "pitch": -0.823811627402435,
+        "fov": 1.4311086604234626
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.427877421124581,
+          "pitch": 0.2337342941903131,
+          "rotation": 0,
+          "target": "1-11267_pano__01"
+        },
+        {
+          "yaw": 2.1471745562136517,
+          "pitch": 0.10958580044601973,
+          "rotation": 0,
+          "target": "6-11215_pano__04"
+        },
+        {
+          "yaw": -1.9873210579545848,
+          "pitch": 0.12427953597025798,
+          "rotation": 0,
+          "target": "0-11227_pano__07"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.591303276125009,
+          "pitch": -1.092199936260684,
+          "title": "Projection Surface",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.2008381683720586,
+          "pitch": 0.034340415257359425,
+          "title": "Control 1",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-11215_pano__04",
+      "name": "11215_PANO__04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.1455824652652495,
+        "pitch": 0.33017099544174755,
+        "fov": 1.4311086604234626
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9719954202000007,
+          "pitch": 0.08238907083024216,
+          "rotation": 0,
+          "target": "2-11255_pano__02"
+        },
+        {
+          "yaw": -0.6990572112356084,
+          "pitch": 0.246909260773732,
+          "rotation": 0,
+          "target": "5-11219_pano__06"
+        },
+        {
+          "yaw": -2.4823840178310768,
+          "pitch": 0.13870344086355146,
+          "rotation": 0,
+          "target": "4-11237_pano__03"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1364805553738346,
+          "pitch": 0.46090648740577755,
+          "title": "Control 3",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6350533474559192,
+          "pitch": -0.7062990627690269,
+          "title": "Projection Surface",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.233727049208589,
+          "pitch": -0.13823807565058388,
+          "title": "Control 2",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Illuminus_BlackMath_Scout_2018",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
